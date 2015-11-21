41e4a36e5212d5463f9d154883567275466fd94e
TrenzenokV <trenzenok-vova@mail.ru>
2015-11-22 00:38:04 +0200

    keeep going

--- src/main/java/TradeOffer.java
+++ src/main/java/TradeOffer.java
@@ -1,10 +1,16 @@
+import java.util.List;
+
 public class TradeOffer {
     public int id;
     public boolean isActive;
+    //Inventory botInventory;   // appID/contextID/itemID/partnerID
+    //Inventory partnerInventory;
+
 
     TradeOffer(int id, boolean isActive)
     {
         this.id = id;
         this.isActive = isActive;
+
     }
 }
