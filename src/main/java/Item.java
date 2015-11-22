4ad560b453b9323b7a4209ee429d0355bfa9b41a
Nikita <infosamara@yandex.ru>
2015-11-22 14:26:33 +0400

    added some http stuff

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,9 +1,15 @@
+import com.sun.org.glassfish.gmbal.NameValue;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.lang.model.element.Name;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Item {
     public String marketHashName;
@@ -14,6 +20,16 @@ public class Item {
     public String itemId;
 
     public String partnerId;
+    public JSONMetrics weaponMetrics;
+
+
+    class JSONMetrics
+    {
+        boolean success;
+        String lowest_price;
+        String volume;
+        String median_price;
+    }
 
     Item(String itemEconomyData) throws IOException {
         String[] parts = itemEconomyData.split("/");
@@ -23,7 +39,10 @@ public class Item {
         itemId = parts[2];
         partnerId = parts[3];
 
+        weaponMetrics = new JSONMetrics();
+
         //Getting market_hash_name of item
+
 
         Document document = Jsoup.parse(BotUser.currentUser.requestor.getAnswer(Requestor.query_type.GET, "https://steamcommunity.com/"
                 + "economy/itemhover/" + appId + "/" + contextId + "/" + itemId + "?o=" + partnerId, null));
@@ -50,8 +69,26 @@ public class Item {
             marketHashName = str.toString();
             i++;
         }
-        System.out.println(marketHashName);
 
+        getMetrics(marketHashName, appId);
 
+        System.out.println(weaponMetrics.lowest_price);
+    }
+
+    void getMetrics(String marketHashName, String appId) throws IOException {
+        String baseURI = "http://steamcommunity.com/market/priceoverview/";
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+        String formattedMarketHashName = marketHashName.replace(" ","%20");
+
+        params.add(new BasicNameValuePair("country", "US"));
+        params.add(new BasicNameValuePair("currency", "1"));
+        params.add(new BasicNameValuePair("appid", appId));
+        params.add(new BasicNameValuePair("market_hash_name", marketHashName));
+
+        String response = BotUser.currentUser.requestor.getAnswer(Requestor.query_type.GET, baseURI + "?" + "country=US&" + "currency=1&" +
+                                                                     "appid=" + appId + "&" + "market_hash_name=" + formattedMarketHashName, null);
+
+        weaponMetrics = BotUser.currentUser.gsonEntity.fromJson(response, JSONMetrics.class);
     }
 }
